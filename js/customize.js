50d23721c731dd00d64d7cf6279a626b5b1cad45
mingshuya <mingshuya@gmail.com>
2024-04-10 19:07:05 +0800

    20240410 html上傳

--- js/customize.js
+++ js/customize.js
@@ -319,7 +319,7 @@ $(function () {
   function _CLOSEMENU() {
     $('aside').removeClass('open');
     $('.overlay').removeClass('show');
-    $('.wrapper').removeClass('noscroll');
+    $('.content').removeClass('noscroll');
     $(this).blur();
 
     //手機版關閉左側選單
@@ -349,7 +349,7 @@ $(function () {
     .click(function (e) {
       $('aside').toggleClass('open');
       $('.overlay').toggleClass('show');
-      $('.wrapper').toggleClass('noscroll');
+      $('.content').toggleClass('noscroll');
       $(this).blur();
       e.preventDefault();
     });
@@ -558,3 +558,81 @@ $(document).ready(function () {
     $('.cat' + $(this).attr('data-prod-cat')).toggle();
   });
 });
+
+// 字級
+/*------------------------------------*/
+/////////////字型大小 font-size//////////
+/*------------------------------------*/
+$('.font_size')
+  .find('.small')
+  .click(function (e) {
+    $(this).parent('li').siblings('li').find('a').removeClass('active');
+    $('.content').removeClass('large_size').addClass('small_size');
+    $(this).blur().addClass('active');
+    e.preventDefault();
+    createCookie('FontSize', 'small', 356);
+  });
+$('.font_size')
+  .find('.medium')
+  .click(function (e) {
+    $(this).parent('li').siblings('li').find('a').removeClass('active');
+    $('.content').removeClass('large_size small_size');
+    $(this).blur().addClass('active');
+    e.preventDefault();
+    createCookie('FontSize', 'medium', 356);
+  });
+$('.font_size')
+  .find('.large')
+  .click(function (e) {
+    $(this).parent('li').siblings('li').find('a').removeClass('active');
+    $('.content').removeClass('small_size').addClass('large_size');
+    $(this).blur().addClass('active');
+    e.preventDefault();
+    createCookie('FontSize', 'large', 356);
+  });
+
+function createCookie(name, value, days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    var expires = '; expires=' + date.toGMTString();
+  } else expires = '';
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
+
+function readCookie(name) {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+window.onload = function (e) {
+  var cookie = readCookie('FontSize');
+  //alert('cookie='+cookie);
+  if (cookie == 'small') {
+    //$('.font_size').find('.small').click();
+    $('.font_size').find('.small').parent('li').siblings('li').find('a').removeClass('active');
+    $('.content').removeClass('large_size medium_size').addClass('small_size');
+    $('.font_size').find('.small').addClass('active');
+    e.preventDefault();
+  } else {
+    if (cookie == 'large') {
+      //$('.font_size').find('.large').click();
+      $('.font_size').find('.large').parent('li').siblings('li').find('a').removeClass('active');
+      $('.content').removeClass('small_size medium_size').addClass('large_size');
+      $('.font_size').find('.large').addClass('active');
+      e.preventDefault();
+    } else {
+      //這裡是預設宣告
+      //$('.font_size').find('.medium').click();
+      $('.font_size').find('.medium').parent('li').siblings('li').find('a').removeClass('active');
+      $('.content').removeClass('large_size small_size');
+      $('.font_size').find('.medium').addClass('active');
+      e.preventDefault();
+    }
+  }
+};
